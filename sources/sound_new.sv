8642bb9283801fabcc56810e3cacd0b97fe56104
m9in1 <kirill000ss56@gmail.com>
2023-06-04 16:35:26 +0300

    New sound pwm

--- sources/sound_new.sv
+++ sources/sound_new.sv
@@ -11,10 +11,10 @@ module sound_new(
 	assign {duty[3], duty[2],duty[1], duty[0]} = aud_en ? {data_i[31:24], data_i[23:16], data_i[15:8], data_i[7:0]} : 0;
 
 	always@(posedge clk) begin
-		if(&cntr!=1) begin
+		
 			cntr<=cntr+1;
 
-		end
+		
 
 
 	end
